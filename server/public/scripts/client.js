beea704e0af51c9fc8adbe935d38aad2c8801eb9
Bryan Highhill <bryanhighhill@gmail.com>
2022-12-07 12:58:49 -0600

    new tasks are successfully appending to dom on page load and on button click. They are appended with two new buttons - one for delete and one for task complete

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -6,7 +6,7 @@ function onReady() {
     console.log('in onReady');
 
     //function to retrieve current task list from server/database
-    // getTasks();
+    getTasks();
 
     //on-click event for submit task button
     $('#submit-task-btn').on('click', submitTask);
@@ -22,7 +22,9 @@ function submitTask() {
         $.ajax({
             method: 'POST',
             url: '/tasks',
-            data: newTask,
+            data: {
+                task: newTask,
+            }
         }).then(function(response){
             console.log('new task POST response from the server', response);
             
@@ -49,4 +51,20 @@ function getTasks(){
         console.log('this is the tasks GET response from the server', response);
         appendToDom(response);
     });
+}
+
+
+//function to append task list to DOM
+function appendToDom(array){
+    console.log('appendToDom function', array);
+    $('#to-do-list').empty();
+    for (let item of array) {
+        $('#to-do-list').append(`
+            <li>${item.task}</li>
+            <button id="completed">Task Complete</button>
+            <button id="delete">Delete Task</button>
+            <br />
+            <br />
+        `)
+    }
 }
