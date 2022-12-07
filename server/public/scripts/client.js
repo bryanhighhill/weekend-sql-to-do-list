5fd432370b9b93d9b28c485d75985613b05d6949
Bryan Highhill <bryanhighhill@gmail.com>
2022-12-07 12:50:43 -0600

    added function to POST user-entered new task upon button click. New task gets posted to server then pushed to the task array. Created GET request that then retrieves the updated array

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -6,7 +6,7 @@ function onReady() {
     console.log('in onReady');
 
     //function to retrieve current task list from server/database
-    getTasks();
+    // getTasks();
 
     //on-click event for submit task button
     $('#submit-task-btn').on('click', submitTask);
@@ -15,21 +15,38 @@ function onReady() {
 //function to POST new task to server
 function submitTask() {
     let newTask = $('#task-input').val();
-    if (newTask != ''){
+    if (newTask === ''){
+        alert('task input cannot be blank');
+        return false;
+    }
         $.ajax({
             method: 'POST',
             url: '/tasks',
             data: newTask,
         }).then(function(response){
             console.log('new task POST response from the server', response);
+            
             //get tasks here
             getTasks();
+            
+            //clear input value here
+            $('#task-input').val('');
+
+
         }).catch(function(error){
             alert(error.responseText);
             console.log(error);
         });
+}
 
-    }
-    alert('task input cannot be blank');
 
+//function to GET tasks from server/database
+function getTasks(){
+    $.ajax({
+        method: 'GET',
+        url: '/tasks',
+    }).then(function(response){
+        console.log('this is the tasks GET response from the server', response);
+        appendToDom(response);
+    });
 }
