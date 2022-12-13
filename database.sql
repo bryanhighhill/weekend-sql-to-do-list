6bd2b98eb1eb109557f5cfd78cab447e191ebd9b
Bryan Highhill <bryanhighhill@gmail.com>
2022-12-13 11:16:06 -0600

    made change to database.sql - removed comments

--- database.sql
+++ database.sql
@@ -1,6 +1,6 @@
 -- my create table SQL queries
 CREATE TABLE "tasks" (
-	"id" serial primary key, --varchar = max number of characters // not null = can't be blank
+	"id" serial primary key,
 	"task" varchar(100) not null,
 	"complete" varchar(10) not null
 );
